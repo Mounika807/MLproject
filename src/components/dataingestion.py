36ad4af80520cda437c1e54a8dadaa33a3f8ec08
Your <mounikareddykamasani313@gmail.com>
2023-09-09 15:37:30 +0530

    modeltrainer added

--- src/components/dataingestion.py
+++ src/components/dataingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from datatransformation import DataTransformation
+from modeltrainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,5 +52,5 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
